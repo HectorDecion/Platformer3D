4d358a9a6ca782093efcc74e308811b33e097720
Hector Oswaldo Decion Robles <hectordecionr@gmail.com>
2023-07-31 00:36:03 -0600

    update plus1
    un error no dejaba terminar el juego

--- Assets/Scripts/PlatformMovementLeftRight.cs
+++ Assets/Scripts/PlatformMovementLeftRight.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlatformMovementLeftRight : MonoBehaviour
@@ -9,10 +10,13 @@ public class PlatformMovementLeftRight : MonoBehaviour
 
     private Vector3 initialPosition;
     private float movementDirection = 1f;
+    public Transform playerPosicion;
+    public Transform playerTP;
 
     private void Start()
     {
         initialPosition = transform.position;
+        playerTP = GetComponent<Transform>();
     }
 
     private void Update()
@@ -25,5 +29,16 @@ public class PlatformMovementLeftRight : MonoBehaviour
         {
             movementDirection *= -1f;
         }
+        playerPosicion = GetComponent<Transform>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerPosicion.transform.position = playerTP.transform.position;
+
+        }
     }
 }
+
